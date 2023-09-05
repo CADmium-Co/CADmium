c82a6488688bba643eb8a15256ffedc5c6926418
Matt Ferraro <mferraro89@gmail.com>
2023-09-04 21:56:57 -0400

    try to build wasm on vercel

---
+++ vercel_build.sh
@@ -0,0 +1,20 @@
+echo "Installing Rustup..."
+# Install Rustup (compiler)
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+# Adding binaries to path
+source "$HOME/.cargo/env"
+
+
+echo "Installing wasm-pack..."
+# Install wasm-pack
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
+
+
+echo "Building wasm-parser..."
+# Build wasm from rust 
+npm run build:wasm
+
+
+echo "Build static frontend client..."
+# Build static html for the react client
+npm run build
