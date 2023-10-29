d2bfec23408a85c572f39f7e4d569110caf7b46f
Matt Ferraro <mferraro89@gmail.com>
2023-10-29 15:13:19 -0400

    add right click dropdown menu

--- src/routes/cadmium/+page.svelte
+++ src/routes/cadmium/+page.svelte
@@ -12,6 +12,77 @@
 	let current_step = {}
 	let main_canvas
 
+	let showMenu = false
+	// pos is cursor position when right click occur
+	let pos = { x: 0, y: 0 }
+	// menu is dimension (height and width) of context menu
+	let menu = { h: 0, y: 0 }
+	// browser/window dimension (height and width)
+	let browser_size = { h: 0, y: 0 }
+
+	function rightClickContextMenu(e) {
+		showMenu = true
+		browser_size = {
+			w: window.innerWidth,
+			h: window.innerHeight
+		}
+		pos = {
+			x: e.clientX,
+			y: e.clientY
+		}
+		// If bottom part of context menu will be displayed
+		// after right-click, then change the position of the
+		// context menu. This position is controlled by `top` and `left`
+		// at inline style.
+		// Instead of context menu is displayed from top left of cursor position
+		// when right-click occur, it will be displayed from bottom left.
+		if (browser_size.h - pos.y < menu.h) pos.y = pos.y - menu.h
+		if (browser_size.w - pos.x < menu.w) pos.x = pos.x - menu.w
+	}
+
+	function onPageClick(e) {
+		// To make context menu disappear when
+		// mouse is clicked outside context menu
+		showMenu = false
+	}
+
+	function getContextMenuDimension(node) {
+		// This function will get context menu dimension
+		// when navigation is shown => showMenu = true
+		let height = node.offsetHeight
+		let width = node.offsetWidth
+		menu = {
+			h: height,
+			w: width
+		}
+	}
+
+	function addItem() {
+		console.log('Add item')
+	}
+
+	function remove() {
+		console.log('remove item')
+	}
+
+	let menuItems = [
+		{
+			name: 'addItem',
+			onClick: addItem,
+			displayText: 'Add Item',
+			class: 'fa-solid fa-plus'
+		},
+		{
+			name: 'hr'
+		},
+		{
+			name: 'trash',
+			onClick: remove,
+			displayText: 'Delete',
+			class: 'fa-solid fa-trash-can'
+		}
+	]
+
 	if (browser) {
 		onMount(() => {
 			init().then(() => {
@@ -227,6 +298,10 @@
 						on:keypress={() => {
 							history_item_onclick(item)
 						}}
+						on:contextmenu|preventDefault={(e) => {
+							console.log('right click', e)
+							rightClickContextMenu(e)
+						}}
 						role="button"
 						tabindex="0"
 					>
@@ -236,9 +311,93 @@
 				{/each}
 			</div>
 		</div>
+		{#if showMenu}
+			<nav use:getContextMenuDimension style="position: absolute; top:{pos.y}px; left:{pos.x}px">
+				<div class="navbar" id="navbar">
+					<ul>
+						{#each menuItems as item}
+							{#if item.name == 'hr'}
+								<hr />
+							{:else}
+								<li>
+									<button on:click={item.onClick}><i class={item.class} />{item.displayText}</button
+									>
+								</li>
+							{/if}
+						{/each}
+					</ul>
+				</div>
+			</nav>
+		{/if}
 	</aside>
 	<main class="h-[100%]">
 		<MainCanvas {realization} bind:this={main_canvas} />
 	</main>
 	<footer class="col-span-2">Footer</footer>
 </div>
+
+<svelte:head>
+	<!-- You can change icon sets according to your taste. Change `class` value in `menuItems` above to represent your icons. -->
+	<!-- <link rel="stylesheet" href="/icon/css/mfglabs_iconset.css"> -->
+	<link
+		rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+		integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+		crossorigin="anonymous"
+		referrerpolicy="no-referrer"
+	/>
+</svelte:head>
+
+<svelte:window on:click={onPageClick} />
+
+<style>
+	.navbar {
+		display: inline-flex;
+		border: 1px #999 solid;
+		width: 170px;
+		background-color: #fff;
+		border-radius: 10px;
+		overflow: hidden;
+		flex-direction: column;
+	}
+	.navbar ul {
+		margin: 6px;
+	}
+	ul li {
+		display: block;
+		list-style-type: none;
+		width: 1fr;
+	}
+	ul li button {
+		font-size: 1rem;
+		color: #222;
+		width: 100%;
+		height: 30px;
+		text-align: left;
+		border: 0px;
+		background-color: #fff;
+	}
+	ul li button:hover {
+		color: #000;
+		text-align: left;
+		border-radius: 5px;
+		background-color: #eee;
+	}
+	ul li button i {
+		padding: 0px 15px 0px 10px;
+	}
+	ul li button i.fa-square {
+		color: #fff;
+	}
+	ul li button:hover > i.fa-square {
+		color: #eee;
+	}
+	ul li button:hover > i.warning {
+		color: crimson;
+	}
+	hr {
+		border: none;
+		border-bottom: 1px solid #ccc;
+		margin: 5px 0px;
+	}
+</style>
