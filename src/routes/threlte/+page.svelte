6918d6efbec57db882798e808a082ab06e73da7a
Matt Ferraro <mferraro89@gmail.com>
2023-12-19 15:40:34 -0500

    get realization on load

--- src/routes/threlte/+page.svelte
+++ src/routes/threlte/+page.svelte
@@ -1,15 +1,51 @@
 <script>
+	import { browser } from '$app/environment'
+	import { onMount } from 'svelte'
+
+	import { default as init, Project } from 'cadmium'
+
 	import AppBar from './AppBar.svelte'
 	import BottomBar from './BottomBar.svelte'
 	import MainDisplay from './MainDisplay.svelte'
 	import ToolBar from './ToolBar.svelte'
+	import {
+		wasmProject,
+		project,
+		realization,
+		wasmRealization,
+		workbench,
+		workbenchIndex,
+		featureIndex,
+		realizationIsStale
+	} from './stores.js'
 
 	let userName = 'mattferraro.dev'
-	let project = { name: 'Threlte Project' }
+
+	if (browser) {
+		onMount(() => {
+			init().then(() => {
+				let p = new Project('First Project')
+				wasmProject.set(p)
+				realizationIsStale.set(true)
+			})
+		})
+	}
+
+	$: if ($realizationIsStale) {
+		console.log('getting new realization')
+		project.set(JSON.parse($wasmProject.json))
+		workbenchIndex.set(0)
+		workbench.set($project.workbenches[$workbenchIndex])
+
+		const maxStep = $featureIndex >= 0 ? $featureIndex + 1 : 1000
+		wasmRealization.set($wasmProject.get_realization(0, maxStep))
+		realization.set(JSON.parse($wasmRealization.to_json()))
+		realizationIsStale.set(false)
+	}
 </script>
 
 <div class="w-[100vw] h-[100vh] block">
-	<AppBar {userName} {project} />
+	<AppBar {userName} project={$project} />
 	<ToolBar />
 	<div class="flex">
 		<MainDisplay />
--- src/routes/threlte/stores.js
+++ src/routes/threlte/stores.js
@@ -2,3 +2,13 @@ import { writable } from 'svelte/store'
 
 export const wasmProject = writable({})
 export const project = writable({})
+
+export const realization = writable({})
+export const wasmRealization = writable({})
+
+export const workbenchIndex = writable(-1)
+export const workbench = writable({ history: [] })
+
+// stores the index of the feature being edited
+export const featureIndex = writable(null)
+export const realizationIsStale = writable(false)
