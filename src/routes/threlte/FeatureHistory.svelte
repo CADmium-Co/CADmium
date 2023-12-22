fe0d54c4b4d2c22f785879671e004a8a953f24a2
Matt Ferraro <mferraro89@gmail.com>
2023-12-22 00:20:50 -0500

    implement rename for extrusion and sketch steps

--- src/routes/threlte/FeatureHistory.svelte
+++ src/routes/threlte/FeatureHistory.svelte
@@ -53,9 +53,9 @@
 				{:else if feature.data.type === 'Plane'}
 					<PlaneFeature name={feature.name} index={featureIdx} />
 				{:else if feature.data.type === 'Sketch'}
-					<SketchFeature name={feature.name} />
+					<SketchFeature name={feature.name} index={featureIdx} />
 				{:else if feature.data.type === 'Extrusion'}
-					<ExtrusionFeature name={feature.name} />
+					<ExtrusionFeature name={feature.name} index={featureIdx} />
 				{:else}
 					TODO: {feature.name} {feature.data.type}
 				{/if}
--- src/routes/threlte/SketchFeature.svelte
+++ src/routes/threlte/SketchFeature.svelte
@@ -1,10 +1,70 @@
 <script>
+	import { slide } from 'svelte/transition'
+	import { quintOut } from 'svelte/easing'
+	import { renameStep } from './projectUtils'
+	import { workbenchIsStale } from './stores.js'
+
 	export let name
+	export let index
+	let editing = false
 
 	let source = '/actions/sketch_min.svg'
+
+	const closeAndRefresh = () => {
+		console.log('closing, refreshing')
+		workbenchIsStale.set(true)
+	}
 </script>
 
-<div class="flex items-center text-sm hover:bg-sky-200" role="button" tabindex="0">
+<div
+	class="flex items-center text-sm hover:bg-sky-200"
+	role="button"
+	tabindex="0"
+	on:dblclick={() => {
+		editing = !editing
+
+		if (editing === false) {
+			closeAndRefresh()
+		}
+	}}
+>
 	<img class="h-8 w-8 px-1" src={source} alt={name} />
 	{name}
 </div>
+
+{#if editing}
+	<div transition:slide={{ delay: 0, duration: 400, easing: quintOut, axis: 'y' }}>
+		<form
+			on:submit|preventDefault={() => {
+				editing = false
+				closeAndRefresh()
+			}}
+			class="px-3 py-2 bg-gray-100 flex flex-col space-y-2"
+			autocomplete="off"
+		>
+			<label>
+				Name
+				<input
+					autocomplete="off"
+					data-1p-ignore
+					class="shadow appearance-none border w-full py-2 px-3 text-gray-700 leading-tight focus:border focus:border-sky-500"
+					bind:value={name}
+				/>
+			</label>
+
+			<div class="flex space-x-1.5">
+				<button
+					class="flex-grow bg-sky-500 hover:bg-sky-700 text-white font-bold py-1.5 px-1 shadow"
+					on:click={() => {
+						renameStep(index, name)
+					}}>Done</button
+				>
+
+				<button
+					class="bg-transparent hover:bg-sky-700 text-sky-500 font-semibold hover:text-white py-1.5 px-4 border border-sky-500 hover:border-transparent"
+					>Cancel</button
+				>
+			</div>
+		</form>
+	</div>
+{/if}
