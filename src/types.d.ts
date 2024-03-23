4f237ef91a32ec2484ed6516ce7ad9375a5f47d3
av8ta <av8ta@protonmail.com>
2024-03-24 09:24:47 +1000

    rearrange types.d.ts file to be ordered like the cadmium.d.ts types that are being generated in rust makes it easier to see what can be changed to line up with cadmium.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -71,136 +71,14 @@ interface WorkBench {
   }
 }
 
-interface PreviewGeometry {
-  type: EntityType
-  start?: PointLikeById
-  end?: PointLikeById
-  center?: PointLikeById
-  radius?: number
-  x?: number
-  y?: number
-  uuid: string
+interface Realization {
+  planes: IDictionary<PlaneRealized>
+  points: IDictionary<Point3D>
+  sketches: IDictionary<SketchTuple>
+  solids: IDictionary<SolidRealized>
 }
 
-interface Plane {
-  origin: Point3D
-  primary: Vector3Like
-  secondary: Vector3Like
-  tertiary: Vector3Like
-}
-
-interface Point {
-  twoD: Vector2
-  threeD: Vector3
-}
-
-interface PointById {
-  twoD: Point2D
-  threeD: Point3D
-  id: string
-}
-
-interface SketchPoint {
-  twoD: Point2D
-  threeD: Point3D
-}
-
-interface Vector2Vector3PointById {
-  twoD: Vector2
-  threeD: Vector3
-  id: string | null // todo !
-}
-
-interface PointLikeById {
-  [x: string]: any // hack todo fix
-  twoD?: Vector2Like | Vector2 | Point2D
-  threeD?: Vector3Like | Vector3 | Point3D
-  id?: string | null
-}
-
-type PointsById = IDictionary<PointById>
-type PointsLikeById = IDictionary<PointLikeById>
-
-interface SnapEntity {
-  id: string
-  type: EntityType
-  x?: number
-  y?: number
-  z?: number
-}
-
-type ProjectToPlane = (point3D: Vector3) => Vector2
-
-interface Point3D {
-  x: number
-  y: number
-  z: number
-  hidden: boolean
-}
-
-interface Point2D {
-  x: number
-  y: number
-  m: number
-  dx: number
-  dy: number
-  fx: number
-  fy: number
-  fixed: boolean
-  hidden: boolean
-}
-
-interface LineTuple {
-  id: string // string number todo convert to numbers
-  start: {
-    twoD: Point2D
-    threeD: Point3D
-  },
-  end: {
-    twoD: Point2D
-    threeD: Point3D
-  }
-}
-
-interface CircleTuple {
-  id: string // string number todo convert to numbers
-  center: {
-    twoD: Point2D
-    threeD: Point3D
-  }
-  radius: number
-}
-
-interface ArcTuple {
-  id: string // string number todo convert to numbers
-  center: {
-    twoD: Point2D
-    threeD: Point3D
-  }
-  start: {
-    twoD: Point2D
-    threeD: Point3D
-  }
-  end: {
-    twoD: Point2D
-    threeD: Point3D
-  }
-}
-
-interface FaceTuple {
-  id: string // string number todo convert to numbers
-  face: object // todo
-  // "face": {
-  //   "exterior": {
-  //     "Circle": {
-  //       "center": 5,
-  //       "radius": 13.76241291178012,
-  //       "top": 7
-  //     }
-  //   },
-  //   "holes": []
-  // }
-}
+type SketchTuple = [SketchRealized, SketchRealized, string]
 
 interface HistoryStep {
   name: string
@@ -236,44 +114,6 @@ interface PlaneData {
   }
 }
 
-interface ExtrusionData {
-  data: {
-    type: HistoryStepType = "Extrusion"
-    extrusion: {
-      sketch_id: string
-      face_ids: number[] // todo should be string
-      length: number
-      offset: number
-      direction: string // e.g "Normal"  todo enums
-      mode: string // e,g "New"  todo enums
-    }
-  }
-}
-
-interface SketchRealized {
-  plane_id: string
-  plane_name: string
-  points: IDictionary<Point3D>
-  points_2d: IDictionary<Point2D>
-  highest_point_id: number
-  line_segments: IDictionary<SegmentId>
-  highest_line_segment_id: number
-  circles: IDictionary<Circle>
-  highest_circle_id: number
-  arcs: IDictionary<Arc> // todo key id is string should be number number
-  highest_arc_id: number
-  constraints: object // todo
-  highest_constraint_id: number
-  faces: array // todo
-}
-
-interface Arc {
-  center: number
-  start: number
-  end: number
-  clockwise: boolean
-}
-
 interface SketchData {
   data: {
     type: HistoryStepType = "Sketch"
@@ -297,9 +137,57 @@ interface SketchData {
   }
 }
 
+export interface Sketch_GeneratedFromRust {
+  points: Record<number, Point2>;
+  highest_point_id: number;
+  line_segments: Record<number, Line2>;
+  highest_line_segment_id: number;
+  circles: Record<number, Circle2>;
+  highest_circle_id: number;
+  arcs: Record<number, Arc2>;
+  highest_arc_id: number;
+  constraints: Record<number, Constraint>;
+  highest_constraint_id: number;
+}
+
+interface ExtrusionData {
+  data: {
+    type: HistoryStepType = "Extrusion"
+    extrusion: {
+      sketch_id: string
+      face_ids: number[] // todo should be string
+      length: number
+      offset: number
+      direction: string // e.g "Normal"  todo enums
+      mode: string // e,g "New"  todo enums
+    }
+  }
+}
+
+interface Plane {
+  origin: Point3D
+  primary: Vector3Like
+  secondary: Vector3Like
+  tertiary: Vector3Like
+}
+
+interface Point3D {
+  x: number
+  y: number
+  z: number
+  hidden: boolean
+}
+
 interface SegmentId {
   start: number
   end: number
+}
+
+interface Arc {
+  center: number
+  start: number
+  end: number
+  clockwise: boolean
 }
 
 interface Circle {
@@ -308,73 +196,21 @@ interface Circle {
   top: number
 }
 
-interface TruckNurbsPoint {
-  x: number
-  y: number
-  z: number
-  w: number
-}
-
-type TruckNurbsSurfaceControlPoint = [TruckNurbsPoint, TruckNurbsPoint] // [start, end] ?
-type TruckNurbsSurfaceControlPoints = TruckNurbsSurfaceControlPoint[]
-
-interface TruckNurbsSurface {
-  NURBSSurface: {
-    knot_vecs: number[][]
-    // knot_vecs: [                              // todo type strongly
-    //   [0, 0, 0, 0.25, 0.5, 0.75, 1, 1, 1],    // Vector3Like[] ?
-    //   [0, 0, 1, 1]                            // similar to NurbsPoint? [x,y,z,w] ?
-    // ],
-    control_points: TruckNurbsSurfaceControlPoints
-  }
-}
-
-interface TruckPlane {
-  Plane: {
-    o: Vector3Like
-    p: Vector3Like
-    q: Vector3Like
-  }
-}
-
-type TruckSurface = TruckNurbsSurface | TruckPlane
-type TruckFaceBoundary = TruckFaceEdgeIndex[]
-
-interface TruckFace {
-  boundaries: TruckFaceBoundary[]
-  orientation: boolean
-  surface: TruckSurface
-}
-
-interface TruckNurbsCurve {
-  NURBSCurve: {
-    knot_vec: number[]
-    // "knot_vec": [0, 0, 0, 0.25, 0.5, 0.75, 1, 1, 1], // todo type strongly
-    control_points: TruckNurbsPoint[]
-  }
-}
-
-type TruckEdgeEndpoints = [number, number] // [startIndex, EndIndex] ?
-
-interface TruckEdge {
-  vertices: TruckEdgeEndpoints
-  curve: TruckCurve
-}
-
-type TruckLineVectors = [Vector3Like, Vector3Like] // [{y,y,z}, {y,y,z}] // [start, end]
-
-interface TruckLine {
-  Line: TruckLineVectors
-}
-
-type TruckCurve = TruckNurbsCurve | TruckLine
-
-type SketchTuple = [SketchRealized, SketchRealized, string]
-interface Realization {
-  planes: IDictionary<PlaneRealized>
+interface SketchRealized {
+  plane_id: string
+  plane_name: string
   points: IDictionary<Point3D>
-  sketches: IDictionary<SketchTuple>
-  solids: IDictionary<SolidRealized>
+  points_2d: IDictionary<Point2D>
+  highest_point_id: number
+  line_segments: IDictionary<SegmentId>
+  highest_line_segment_id: number
+  circles: IDictionary<Circle>
+  highest_circle_id: number
+  arcs: IDictionary<Arc> // todo key id is string should be number number
+  highest_arc_id: number
+  constraints: object // todo
+  highest_constraint_id: number
+  faces: array // todo
 }
 
 interface PlaneRealized {
@@ -383,65 +219,6 @@ interface PlaneRealized {
   height: number
   plane: Plane
 }
-
-interface TruckFaceEdgeIndex {
-  index: number
-  orientation: boolean
-}
-
-interface TruckBoundary {
-  vertices: Vector3Like[]
-  edges: TruckEdge[]
-  faces: TruckFace[]
-}
-
-interface TruckSolid {
-  boundaries: TruckBoundary[]
-}
-interface SolidRealized {
-  name: string
-  crc32: string
-  vertices: Vector3Like[]
-  normals: Vector3Like[]
-  uvs: Vector3Like[]
-  indices: number[]
-  triangles: array // todo
-  truck_solid: TruckSolid
-}
-
-
-interface ExtrusionSketchData {
-  sketch_id: string
-  face_ids: string[]
-  length: string // todo change to number
-  offset: number
-  direction: string
-  mode: string
-}
-
-// rust expects:
-// `RenameWorkbench`
-// `RenameStep`
-// `RenameProject`
-// `DeleteLines`
-// `DeleteArcs`
-// `DeleteCircles`
-// `NewPointOnSketch`
-// `NewPointOnSketch2`
-// `NewCircleBetweenPoints`
-// `NewRectangleBetweenPoints`
-// `NewLineOnSketch`
-// `DeleteLineSegment`
-// `StepSketch`
-// `SolveSketch`
-// `NewSketchOnPlane`
-// `SetSketchPlane`
-// `DeleteSketch`
-// `NewExtrusion`
-// `UpdateExtrusion`
-// `UpdateExtrusionLength`
-
-// todo these interfaces should be exported from rust with wasm bindgen ?
 
 // ========= io messages sent to rust =========
 interface UpdateExtrusion {
@@ -530,6 +307,28 @@ interface RenameStep {
   new_name: string
 }
 
+export type Message_GeneratedFromRust =
+  | { RenameWorkbench: { workbench_id: number; new_name: string } }
+  | { RenameStep: { workbench_id: number; step_id: number; new_name: string } }
+  | { RenameProject: { new_name: string } }
+  | { DeleteLines: { workbench_id: number; sketch_id: string; line_ids: number[] } }
+  | { DeleteArcs: { workbench_id: number; sketch_id: string; arc_ids: number[] } }
+  | { DeleteCircles: { workbench_id: number; sketch_id: string; circle_ids: number[] } }
+  | { NewPointOnSketch: { workbench_id: number; sketch_id: string; point_id: number; x: number; y: number } } 
+  | { NewPointOnSketch2: { workbench_id: number; sketch_id: string; x: number; y: number; hidden: boolean } } 
+  | { NewCircleBetweenPoints: { workbench_id: number; sketch_id: string; center_id: number; edge_id: number } } 
+  | { NewRectangleBetweenPoints: { workbench_id: number; sketch_id: string; start_id: number; end_id: number } } 
+  | { NewLineOnSketch: { workbench_id: number; sketch_id: string; start_point_id: number; end_point_id: number } } 
+  | { DeleteLineSegment: { workbench_id: number; sketch_name: string; line_segment_id: number } } 
+  | { StepSketch: { workbench_id: number; sketch_name: string; steps: number } } 
+  | { SolveSketch: { workbench_id: number; sketch_name: string; max_steps: number } } 
+  | { NewSketchOnPlane: { workbench_id: number; sketch_name: string; plane_id: string } } 
+  | { SetSketchPlane: { workbench_id: number; sketch_id: string; plane_id: string } } 
+  | { DeleteSketch: { workbench_id: number; sketch_name: string } } 
+  | { NewExtrusion: { workbench_id: number; extrusion_name: string; sketch_id: string; face_ids: number[]; length: number; offset: number; direction: Direction } } 
+  | { UpdateExtrusion: { workbench_id: number; extrusion_name: string; extrusion_id: string; sketch_id: string; face_ids: number[]; length: number; offset: number; direction: Direction } } 
+  | { UpdateExtrusionLength: { workbench_id: number; extrusion_name: string; length: number } }
+
 type Message =
   | { UpdateExtrusion: UpdateExtrusion }
   | { SetSketchPlane: SetSketchPlane }
@@ -548,3 +347,236 @@ interface MessageHistory {
   message: Message
   result: any
 }
+
+interface Point2D {
+  x: number
+  y: number
+  m: number
+  dx: number
+  dy: number
+  fx: number
+  fy: number
+  fixed: boolean
+  hidden: boolean
+}
+
+interface TruckSolid {
+  boundaries: TruckBoundary[]
+}
+
+interface SolidRealized {
+  name: string
+  crc32: string
+  vertices: Vector3Like[]
+  normals: Vector3Like[]
+  uvs: Vector3Like[]
+  indices: number[]
+  triangles: array // todo
+  truck_solid: TruckSolid
+}
+
+interface ExtrusionSketchData {
+  sketch_id: string
+  face_ids: string[]
+  length: string // todo change to number
+  offset: number
+  direction: string
+  mode: string
+}
+
+interface PreviewGeometry {
+  type: EntityType
+  start?: PointLikeById
+  end?: PointLikeById
+  center?: PointLikeById
+  radius?: number
+  x?: number
+  y?: number
+  uuid: string
+}
+
+interface Point {
+  twoD: Vector2
+  threeD: Vector3
+}
+
+interface PointById {
+  twoD: Point2D
+  threeD: Point3D
+  id: string
+}
+
+interface SketchPoint {
+  twoD: Point2D
+  threeD: Point3D
+}
+
+interface PointLikeById {
+  [x: string]: any // hack todo fix
+  twoD?: Vector2Like | Vector2 | Point2D
+  threeD?: Vector3Like | Vector3 | Point3D
+  id?: string | null
+}
+
+type PointsById = IDictionary<PointById>
+type PointsLikeById = IDictionary<PointLikeById>
+
+interface SnapEntity {
+  id: string
+  type: EntityType
+  x?: number
+  y?: number
+  z?: number
+}
+
+type ProjectToPlane = (point3D: Vector3) => Vector2
+
+interface LineTuple {
+  id: string // string number todo convert to numbers
+  start: {
+    twoD: Point2D
+    threeD: Point3D
+  },
+  end: {
+    twoD: Point2D
+    threeD: Point3D
+  }
+}
+
+interface CircleTuple {
+  id: string // string number todo convert to numbers
+  center: {
+    twoD: Point2D
+    threeD: Point3D
+  }
+  radius: number
+}
+
+interface ArcTuple {
+  id: string // string number todo convert to numbers
+  center: {
+    twoD: Point2D
+    threeD: Point3D
+  }
+  start: {
+    twoD: Point2D
+    threeD: Point3D
+  }
+  end: {
+    twoD: Point2D
+    threeD: Point3D
+  }
+}
+
+interface FaceTuple {
+  id: string // string number todo convert to numbers
+  face: object // todo
+  // "face": {
+  //   "exterior": {
+  //     "Circle": {
+  //       "center": 5,
+  //       "radius": 13.76241291178012,
+  //       "top": 7
+  //     }
+  //   },
+  //   "holes": []
+  // }
+}
+
+interface TruckNurbsPoint {
+  x: number
+  y: number
+  z: number
+  w: number
+}
+
+type TruckNurbsSurfaceControlPoint = [TruckNurbsPoint, TruckNurbsPoint] // [start, end] ?
+type TruckNurbsSurfaceControlPoints = TruckNurbsSurfaceControlPoint[]
+
+interface TruckNurbsSurface {
+  NURBSSurface: {
+    knot_vecs: number[][]
+    // knot_vecs: [                              // todo type strongly
+    //   [0, 0, 0, 0.25, 0.5, 0.75, 1, 1, 1],    // Vector3Like[] ?
+    //   [0, 0, 1, 1]                            // similar to NurbsPoint? [x,y,z,w] ?
+    // ],
+    control_points: TruckNurbsSurfaceControlPoints
+  }
+}
+
+interface TruckPlane {
+  Plane: {
+    o: Vector3Like
+    p: Vector3Like
+    q: Vector3Like
+  }
+}
+
+type TruckSurface = TruckNurbsSurface | TruckPlane
+type TruckFaceBoundary = TruckFaceEdgeIndex[]
+
+interface TruckFace {
+  boundaries: TruckFaceBoundary[]
+  orientation: boolean
+  surface: TruckSurface
+}
+
+interface TruckNurbsCurve {
+  NURBSCurve: {
+    knot_vec: number[]
+    // "knot_vec": [0, 0, 0, 0.25, 0.5, 0.75, 1, 1, 1], // todo type strongly
+    control_points: TruckNurbsPoint[]
+  }
+}
+
+type TruckEdgeEndpoints = [number, number] // [startIndex, EndIndex] ?
+
+interface TruckEdge {
+  vertices: TruckEdgeEndpoints
+  curve: TruckCurve
+}
+
+type TruckLineVectors = [Vector3Like, Vector3Like] // [{y,y,z}, {y,y,z}] // [start, end]
+
+interface TruckLine {
+  Line: TruckLineVectors
+}
+
+type TruckCurve = TruckNurbsCurve | TruckLine
+
+interface TruckFaceEdgeIndex {
+  index: number
+  orientation: boolean
+}
+
+interface TruckBoundary {
+  vertices: Vector3Like[]
+  edges: TruckEdge[]
+  faces: TruckFace[]
+}
+
+
+
+// rust expects:
+// `RenameWorkbench`
+// `RenameStep`
+// `RenameProject`
+// `DeleteLines`
+// `DeleteArcs`
+// `DeleteCircles`
+// `NewPointOnSketch`
+// `NewPointOnSketch2`
+// `NewCircleBetweenPoints`
+// `NewRectangleBetweenPoints`
+// `NewLineOnSketch`
+// `DeleteLineSegment`
+// `StepSketch`
+// `SolveSketch`
+// `NewSketchOnPlane`
+// `SetSketchPlane`
+// `DeleteSketch`
+// `NewExtrusion`
+// `UpdateExtrusion`
+// `UpdateExtrusionLength`
+
+// todo these interfaces should be exported from rust with wasm bindgen ?
