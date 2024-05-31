66c7e2622f147e027139ce87af39e8ab7422eac4
Matt Ferraro <MattFerraro@users.noreply.github.com>
2024-05-30 20:54:31 -0400

    pull in TrackballControls as CadControls

---
+++ applications/web/src/components/useControlsContext.ts
@@ -0,0 +1,23 @@
+import {useThrelteUserContext} from "@threlte/core"
+import {writable, type Writable} from "svelte/store"
+import type {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js"
+import type {TrackballControls} from "three/examples/jsm/controls/TrackballControls.js"
+
+type ControlsContext = {
+  orbitControls: Writable<OrbitControls | undefined>
+  trackballControls: Writable<TrackballControls | undefined>
+}
+
+/**
+ * ### `useControlsContext`
+ *
+ * This hook is used to register the `OrbitControls` or `TrackballControls instance
+ * with the `ControlsContext`. We're using this context to enable and disable the
+ * controls when the user is interacting with the TransformControls.
+ */
+export const useControlsContext = (): ControlsContext => {
+  return useThrelteUserContext<ControlsContext>("threlte-controls", {
+    orbitControls: writable<OrbitControls | undefined>(undefined),
+    trackballControls: writable<TrackballControls | undefined>(undefined),
+  })
+}
