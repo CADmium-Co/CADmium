157860a1e75620801977766f2befab4fcc7c72d5
Dimitris Zervas <dzervas@dzervas.gr>
2024-06-03 21:28:37 +0300

    Start ironing out the steps + history
    Signed-off-by: Dimitris Zervas <dzervas@dzervas.gr>

--- packages/cadmium/src/message/mod.rs
+++ packages/cadmium/src/message/mod.rs
@@ -4,6 +4,7 @@ pub mod idwrap;
 pub mod message;
 
 pub use message::{Message, MessageResult};
+use serde::{Deserialize, Serialize};
 
 pub trait Identifiable: Sized {
     type Parent;
@@ -15,7 +16,7 @@ pub trait ProjectMessageHandler: wasm_bindgen::convert::RefFromWasmAbi {
     fn handle_project_message(&self, project: &mut crate::project::Project) -> anyhow::Result<Option<IDType>>;
 }
 
-pub trait MessageHandler {
+pub trait MessageHandler: Serialize + for<'de> Deserialize<'de> + wasm_bindgen::convert::RefFromWasmAbi {
     type Parent: Identifiable;
     fn handle_message(&self, item: Self::Parent) -> anyhow::Result<Option<IDType>>;
 }
