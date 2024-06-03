c8dabf80cbc1af802040c33859561e51475f624d
Dimitris Zervas <dzervas@dzervas.gr>
2024-06-03 23:36:23 +0300

    Record messages in history
    Signed-off-by: Dimitris Zervas <dzervas@dzervas.gr>

--- packages/cadmium/src/step.rs
+++ packages/cadmium/src/step.rs
@@ -8,38 +8,33 @@ use crate::message::Message;
 use crate::realization::Realizable;
 use crate::IDType;
 
-#[derive(Tsify, Debug, Clone, Serialize, Deserialize)]
-#[tsify(into_wasm_abi, from_wasm_abi)]
-pub enum StepOperation {
-    Add,
-    Update,
-    Delete,
-}
-
-impl Display for StepOperation {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            StepOperation::Add => write!(f, "Add"),
-            StepOperation::Update => write!(f, "Update"),
-            StepOperation::Delete => write!(f, "Delete"),
-        }
-    }
-}
-
 #[derive(Tsify, Clone, Debug, Serialize, Deserialize)]
 #[tsify(into_wasm_abi, from_wasm_abi)]
 pub struct Step {
     pub(crate) id: IDType,
-    pub(crate) operation: StepOperation,
     pub(crate) name: String,
     pub(crate) suppressed: bool,
     pub(crate) data: Message,
 }
 
 impl Step {
+    pub fn new(id: IDType, data: Message) -> Self {
+        Step {
+            id,
+            name: "TODO".to_string(),
+            suppressed: false,
+            data,
+        }
+    }
+
     pub fn unique_id(&self) -> String {
-        // TODO: Should use the type of StepData instead of name
-        format!("{}:{}-{}", self.operation, self.name, self.id)
+        format!("{}", self)
+    }
+}
+
+impl Display for Step {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}:{}-{}", self.name, self.data, self.id)
     }
 }
 
